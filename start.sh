051a383a95646a806d18d29d64d1c6de205f66c5
frenol021 <frenol021@gmail.com>
2025-05-12 00:18:45 +0300

    updated config files

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# Ensure Laravel has proper permissions
+chmod -R 775 storage bootstrap/cache
+
 # Replace $PORT in nginx.conf
 envsubst '$PORT' < /app/config/nginx.conf > /app/config/nginx.final.conf
 
-# Start PHP and Nginx
+# Start PHP-FPM and Nginx
 php-fpm -y /app/config/php-fpm.conf &
 nginx -c /app/config/nginx.final.conf -g "daemon off;"
