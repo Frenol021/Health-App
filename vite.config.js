2fa29b834daf5bb9ea85c37968b5fba90ab10944
frenol021 <frenol021@gmail.com>
2025-05-12 10:26:39 +0300

    updated files

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // Make sure build files go to public/build
+        manifest: true,  // Enable the generation of a manifest.json file
+    },
 });
+
