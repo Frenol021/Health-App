bcf26639b8dc1c425f6ceff131959c555c7cf930
frenol021 <frenol021@gmail.com>
2025-05-12 14:03:45 +0300

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  server: {
-    https: true, // Ensure Vite runs in HTTPS mode locally
-  },
-  base: '/build/', // Ensure assets are served from the correct base URL
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    base: process.env.NODE_ENV === 'production' ? '/build/' : '/', // Ensure the correct path in production
 });
-
