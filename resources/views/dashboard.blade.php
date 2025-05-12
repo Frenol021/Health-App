8d73d3eca6c7d426ef155fba7b32015729a88f97
frenol021 <frenol021@gmail.com>
2025-05-12 15:22:28 +0300

    added CSRF token

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,6 +32,7 @@
                     <div class="container mt-6">
 
                         <form method="GET" action="{{ route('clients.name') }}" class="max-w-md mx-auto">
+                             @csrf
                             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                             <div class="relative">
                                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
