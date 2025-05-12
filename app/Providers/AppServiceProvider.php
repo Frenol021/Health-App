f15221b89a9eaa1c29d9dc02d59824871ef2c092
frenol021 <frenol021@gmail.com>
2025-05-12 14:52:53 +0300

    Force HTTPS URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+                // Force HTTPS URLs in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Route::middleware('api')
             ->prefix('api') // Optional: Adds `/api` prefix
             ->group(base_path('routes/api.php'));
